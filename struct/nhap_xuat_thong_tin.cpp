50e9d9830c6256bd61a781ea10f62d7fca2a2bcf
Kiet Hoang <kiethoang101@gmail.com>
2021-09-29 13:39:21 +0700

    add funtion input

--- struct/nhap_xuat_thong_tin.cpp
+++ struct/nhap_xuat_thong_tin.cpp
@@ -4,7 +4,26 @@ struct sinh_vien
 {
     std::string ho_ten;
     std::string mssv;
+    int tuoi;
+    int diem;
 };
+
+void nhap_1_sinh_vien(sinh_vien sv)
+{
+    std::cout << "\nNhap ho ten sinh vien: ";
+    std::cin.ignore();
+    std::getline(std::cin, sv.ho_ten);
+
+    std::cout << "\nNhap ma so sinh vien: ";
+    std::cin.ignore();
+    std::getline(std::cin, sv.mssv);
+
+    std::cout << "\nNhap tuoi: ";
+    std::cin >> sv.tuoi;
+
+    std::cout << "\nNhap diem: ";
+    std::cin >> sv.diem;
+}
 
 int main()
 {
