7314a1d36b01115c775297781cc40e7a7c8246fc
Kiet Hoang <kiethoang101@gmail.com>
2021-09-29 13:51:43 +0700

    Add function output

--- struct/nhap_xuat_thong_tin.cpp
+++ struct/nhap_xuat_thong_tin.cpp
@@ -8,14 +8,14 @@ struct sinh_vien
     int diem;
 };
 
-void nhap_1_sinh_vien(sinh_vien sv)
+void nhap_1_sinh_vien(sinh_vien &sv)
 {
     std::cout << "\nNhap ho ten sinh vien: ";
-    std::cin.ignore();
+    fflush(stdin);
     std::getline(std::cin, sv.ho_ten);
 
     std::cout << "\nNhap ma so sinh vien: ";
-    std::cin.ignore();
+    fflush(stdin);
     std::getline(std::cin, sv.mssv);
 
     std::cout << "\nNhap tuoi: ";
@@ -25,8 +25,24 @@ void nhap_1_sinh_vien(sinh_vien sv)
     std::cin >> sv.diem;
 }
 
+void xuat_1_sinh_vien(sinh_vien sv)
+{
+    std::cout << "\nHo ten sinh vien: " << sv.ho_ten;
+
+    std::cout << "\nMa so sinh vien: " << sv.mssv;
+
+    std::cout << "\nTuoi: " << sv.tuoi;
+
+    std::cout << "\nDiem: " << sv.diem;
+}
+
 int main()
 {
+    sinh_vien sv;
+    std::cout << "\n\n\t\tNHAP THONG TIN 1 SINH VIEN\n";
+    nhap_1_sinh_vien(sv);
 
+    std::cout << "\n\n\t\tXUAT THONG TIN 1 SINH VIEN\n";
+    xuat_1_sinh_vien(sv);
     return 0;
 }
