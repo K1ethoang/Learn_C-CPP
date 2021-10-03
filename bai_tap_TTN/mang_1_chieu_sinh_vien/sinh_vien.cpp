5e52f64ebf00efda4a7cfc9b11da65f111483e0b
Hoang Gia Kiet <kiethoang101@gmail.com>
2021-10-03 19:00:30 +0700

    Update

--- bai_tap_TTN/mang_1_chieu_sinh_vien/sinh_vien.cpp
+++ bai_tap_TTN/mang_1_chieu_sinh_vien/sinh_vien.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 
+struct sinh_vien;
+struct danh_sach_sinh_vien;
 // * Khai báo cấu trúc sinh viên
 struct sinh_vien
 {
@@ -10,6 +12,106 @@ struct sinh_vien
     float diem_toan_roi_rac;
     float diem_CTDL_GT;
 };
+
+// * Khai bao danh sach thong tin sinh vien
+struct danh_sach_sinh_vien
+{
+    sinh_vien array_sv[100]; // * Khai báo mảng 1 chiều tĩnh chứa 100 sinh viên
+    int n;                   // * Số lượng sinh viên hiện có trong mảng
+};
+
+void nhap_thong_tin_1_sinh_vien(sinh_vien &sv);
+void xuat_thong_tin_1_sinh_vien(sinh_vien sv);
+void nhap_thong_tin_nhieu_sinh_vien(danh_sach_sinh_vien &ds);
+void xuat_danh_sach_sinh_vien(danh_sach_sinh_vien ds);
+float tinh_diem_trung_binh(sinh_vien sv);
+void xep_loai_1_sinh_vien(sinh_vien sv);
+void xep_loai_danh_sach_sinh_vien(danh_sach_sinh_vien ds);
+void hoan_vi(sinh_vien &x, sinh_vien &y);
+void sap_xep_tang_dan_theo_diem_tb(danh_sach_sinh_vien &ds);
+void sap_xep_giam_dan_theo_ten(danh_sach_sinh_vien &ds);
+
+int main()
+{
+    danh_sach_sinh_vien ds_sinh_vien;
+    bool thoat = false; // * Thiết lập ban đầu là không thoát
+
+    do
+    {
+        system("cls");
+        short lua_chon;
+        std::cout << "\n=================================== MENU ===================================";
+        std::cout << "\n\t1. Nhap danh sach sinh vien";
+        std::cout << "\n\t2. Xuat danh sach sinh vien";
+        std::cout << "\n\t3. Xep loai sinh vien";
+        std::cout << "\n\t4. Sap xep danh sach sinh vien TANG DAN theo DIEM TRUNG BINH";
+        std::cout << "\n\t5. Sap xep danh sach sinh vien GIAM DAN theo TEN";
+        std::cout << "\n\t0. Thoat chuong trinh";
+        std::cout << "\n============================================================================";
+        std::cout << "\nNhap lua chon cua ban: ";
+
+        std::cin >> lua_chon;
+        switch (lua_chon)
+        {
+        case 0:
+            std::cout << "\nBan da chon thoat chuong trinh !";
+            system("pause");
+            thoat = true;
+            break;
+        case 1:
+            system("cls");
+            std::cout << "\n\n\t\t\t\t1. NHAP DANH SACH SINH VIEN\n";
+            do
+            {
+                std::cout << "\nNhap so luong sinh vien: ";
+                std::cin >> ds_sinh_vien.n;
+                if (ds_sinh_vien.n < 0 || ds_sinh_vien.n > 100)
+
+                {
+                    std::cout << "\n\tSo luong khong hop le! Vui long nhap lai (toi da 100 sinh vien)";
+                }
+            } while (ds_sinh_vien.n < 0 || ds_sinh_vien.n > 100);
+            nhap_thong_tin_nhieu_sinh_vien(ds_sinh_vien);
+            std::cout << "\n\n\t\t\t\tDANH SACH SINH VIEN VUA NHAP\n";
+            xuat_danh_sach_sinh_vien(ds_sinh_vien);
+            system("pause");
+
+            break;
+        case 2:
+            system("cls");
+            std::cout << "\n\n\t\t\t\t2. XUAT DANH SACH SINH VIEN\n";
+            xuat_danh_sach_sinh_vien(ds_sinh_vien);
+            system("pause");
+            break;
+        case 3:
+            system("cls");
+            std::cout << "\n\n\t\t\t\t3. XEP LOAI SINH VIEN\n";
+            xep_loai_danh_sach_sinh_vien(ds_sinh_vien);
+            system("pause");
+            break;
+        case 4:
+            system("cls");
+            std::cout << "\n\n\t\t\t\t4. DANH SACH SINH VIEN SAP XEP TANG DAN THEO DIEM TB\n";
+            sap_xep_tang_dan_theo_diem_tb(ds_sinh_vien);
+            xuat_danh_sach_sinh_vien(ds_sinh_vien);
+            system("pause");
+            break;
+        case 5:
+            system("cls");
+            std::cout << "\n\n\t\t\t\t5. DANH SACH SAU KHI SAP XEP GIAM DAN THEO TEN\n";
+            sap_xep_giam_dan_theo_ten(ds_sinh_vien);
+            xuat_danh_sach_sinh_vien(ds_sinh_vien);
+            system("pause");
+            break;
+        default:
+            std::cout << "\nLua chon cua ban khon hop le !";
+            system("pause");
+            break;
+        }
+    } while (!thoat);
+
+    return 0;
+}
 
 // * Hàm nhập thông tin 1 sinh viên
 void nhap_thong_tin_1_sinh_vien(sinh_vien &sv)
@@ -49,7 +151,7 @@ void nhap_thong_tin_1_sinh_vien(sinh_vien &sv)
 
     do
     {
-        std::cout << "\n\tNhap Diem Toan Roi Rac: ";
+        std::cout << "\nNhap Diem Toan Roi Rac: ";
         std::cin >> sv.diem_toan_roi_rac;
         if (sv.diem_toan_roi_rac < 0 || sv.diem_toan_roi_rac > 10)
         {
@@ -77,13 +179,6 @@ void xuat_thong_tin_1_sinh_vien(sinh_vien sv)
     std::cout << "\nDiem Toan Roi Rac: " << sv.diem_toan_roi_rac;
     std::cout << "\nDiem Cau Truc Du Lieu & Giai Thuat: " << sv.diem_CTDL_GT;
 }
-
-// * Khai bao danh sach thong tin sinh vien
-struct danh_sach_sinh_vien
-{
-    sinh_vien array_sv[100]; // * Khai báo mảng 1 chiều tĩnh chứa 100 sinh viên
-    int n;                   // * Số lượng sinh viên hiện có trong mảng
-};
 
 // ! Ham nhap danh sach thong tin sinh vien
 void nhap_thong_tin_nhieu_sinh_vien(danh_sach_sinh_vien &ds)
@@ -188,86 +283,4 @@ void sap_xep_giam_dan_theo_ten(danh_sach_sinh_vien &ds)
             }
         }
     }
-}
-
-int main()
-{
-    danh_sach_sinh_vien ds_sinh_vien;
-    bool thoat = false; // * Thiết lập ban đầu là không thoat
-
-    do
-    {
-        system("cls");
-        short lua_chon;
-        std::cout << "\n=================================== MENU ===================================";
-        std::cout << "\n\t1. Nhap danh sach sinh vien";
-        std::cout << "\n\t2. Xuat danh sach sinh vien";
-        std::cout << "\n\t3. Xep loai sinh vien";
-        std::cout << "\n\t4. Sap xep danh sach sinh vien TANG DAN theo DIEM TRUNG BINH";
-        std::cout << "\n\t5. Sap xep danh sach sinh vien GIAM DAN theo TEN";
-        std::cout << "\n\t0. Thoat chuong trinh";
-        std::cout << "\n============================================================================";
-        std::cout << "\nNhap lua chon cua ban: ";
-
-        std::cin >> lua_chon;
-        switch (lua_chon)
-        {
-        case 0:
-            std::cout << "\nBan da chon thoat chuong trinh !";
-            system("pause");
-            thoat = true;
-            break;
-        case 1:
-            system("cls");
-            std::cout << "\n\n\t\t\t\t1. NHAP DANH SACH SINH VIEN\n";
-            do
-            {
-                std::cout << "\nNhap so luong sinh vien: ";
-                std::cin >> ds_sinh_vien.n;
-                if (ds_sinh_vien.n < 0 || ds_sinh_vien.n > 100)
-
-                {
-                    std::cout << "\n\tSo luong khong hop le! Vui long nhap lai (toi da 100 sinh vien)";
-                }
-            } while (ds_sinh_vien.n < 0 || ds_sinh_vien.n > 100);
-            nhap_thong_tin_nhieu_sinh_vien(ds_sinh_vien);
-            std::cout << "\n\n\t\t\t\tDANH SACH SINH VIEN VUA NHAP\n";
-            xuat_danh_sach_sinh_vien(ds_sinh_vien);
-            system("pause");
-
-            break;
-        case 2:
-            system("cls");
-            std::cout << "\n\n\t\t\t\t2. XUAT DANH SACH SINH VIEN\n";
-            xuat_danh_sach_sinh_vien(ds_sinh_vien);
-            system("pause");
-            break;
-        case 3:
-            system("cls");
-            std::cout << "\n\n\t\t\t\t3. XEP LOAI SINH VIEN\n";
-            xep_loai_danh_sach_sinh_vien(ds_sinh_vien);
-            system("pause");
-            break;
-        case 4:
-            system("cls");
-            std::cout << "\n\n\t\t\t\t4. DANH SACH SINH VIEN SAP XEP TANG DAN THEO DIEM TB\n";
-            sap_xep_tang_dan_theo_diem_tb(ds_sinh_vien);
-            xuat_danh_sach_sinh_vien(ds_sinh_vien);
-            system("pause");
-            break;
-        case 5:
-            system("cls");
-            std::cout << "\n\n\t\t\t\t5. DANH SACH SAU KHI SAP XEP GIAM DAN THEO TEN\n";
-            sap_xep_giam_dan_theo_ten(ds_sinh_vien);
-            xuat_danh_sach_sinh_vien(ds_sinh_vien);
-            system("pause");
-            break;
-        default:
-            std::cout << "\nLua chon cua ban khon hop le !";
-            system("pause");
-            break;
-        }
-    } while (!thoat);
-
-    return 0;
 }
