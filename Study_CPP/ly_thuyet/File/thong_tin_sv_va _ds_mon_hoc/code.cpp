bfa76a77b06edb88a2caaab290410152e3a83ecd
Hoang Gia Kiet <kiethoang101@gmail.com>
2021-12-25 23:17:50 +0700

    add cau 2

--- Study_CPP/ly_thuyet/File/thong_tin_sv_va _ds_mon_hoc/code.cpp
+++ Study_CPP/ly_thuyet/File/thong_tin_sv_va _ds_mon_hoc/code.cpp
@@ -3,16 +3,17 @@ using namespace std;
 #include <fstream>
 #include <string>
 #include <vector>
+#include <cstring>
 
 struct Subject
 {
-    string name;
+    string fullname;
     float mark;
 };
 
 struct Student
 {
-    string name;
+    string fullname;
     string code;
     // int date, month, year;
     string yearOfBirth;
@@ -25,6 +26,9 @@ void importStudens(ifstream &fileIn, vector<Student> &s);
 void outputOneStudent(Student s);
 void outputStudents(vector<Student> s);
 
+// câu 2: hàm xuất tất cả thồng tin của sinh viên có tên trùng
+void findStudentByFullname(vector<Student> s, string fullname);
+
 int main()
 {
     vector<Student> students;
@@ -34,13 +38,18 @@ int main()
     importStudens(fileIn, students);
     outputStudents(students);
 
+    string fullname;
+    cout << "\nNhap ho ten sinh vien: ";
+    getline(cin, fullname);
+    findStudentByFullname(students, fullname);
+
     fileIn.close();
     return 0;
 }
 
 void readStudent(ifstream &fileIn, Student &s)
 {
-    getline(fileIn, s.name, ','); // đọc dữ liệu đến dấu phẩy thì dừng
+    getline(fileIn, s.fullname, ','); // đọc dữ liệu đến dấu phẩy thì dừng
     // fileIn.seekg(1, ios::cur);
     getline(fileIn, s.code, ',');
     // fileIn.seekg(1, ios::cur);
@@ -54,7 +63,7 @@ void readStudent(ifstream &fileIn, Student &s)
 
 void readSubject(ifstream &fileIn, Subject &s)
 {
-    getline(fileIn, s.name, '-'); // đọc dữ liệu đến dấu '-'
+    getline(fileIn, s.fullname, '-'); // đọc dữ liệu đến dấu '-'
     fileIn >> s.mark;
 }
 
@@ -69,13 +78,20 @@ void importStudens(ifstream &fileIn, vector<Student> &students)
         // * bước 2: đọc số lượng môn học sv đã học
         int n = 0; // số lượng môn học
         fileIn >> n;
+
+        // có cái này để đọc kí tự '\n' của cái dòng bên trên
+        string temp;
+        getline(fileIn, temp);
+
         // * bước 3: đọc danh sách môn học
         for (int i = 0; i < n; i++)
         {
             Subject subject;
             readSubject(fileIn, subject);
+            getline(fileIn, temp);
             student.subjects.push_back(subject); // thêm 1 môn học
         }
+
         // * bước 4: thêm thông tin sinh viên vào vector sinh viên
         students.push_back(student); // thêm 1 sv vào vector
     }
@@ -83,14 +99,14 @@ void importStudens(ifstream &fileIn, vector<Student> &students)
 
 void outputOneStudent(Student s)
 {
-    cout << "\nHo ten: " << s.name << endl;
+    cout << "\nHo ten: " << s.fullname << endl;
     cout << "Ma SV: " << s.code << endl;
     cout << "Nam sinh: " << s.yearOfBirth << endl;
     // cout << "Ngay sinh: " << s.date << "/" << s.month << "/" << s.year;
     // xuất danh sách các môn học
     for (int i = 0; i < s.subjects.size(); i++)
     {
-        cout << "Ten mon hoc: " << s.subjects[i].name << endl;
+        cout << "Ten mon hoc: " << s.subjects[i].fullname << endl;
         cout << "Diem: " << s.subjects[i].mark << endl;
     }
 }
@@ -101,5 +117,17 @@ void outputStudents(vector<Student> s)
     {
         cout << "\n\n\t\tSINH VIEN THU " << i + 1 << endl;
         outputOneStudent(s[i]);
+    }
+}
+
+void findStudentByFullname(vector<Student> s, string fullname)
+{
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (stricmp(s[i].fullname.c_str(), fullname.c_str()) == 0) // hàm ko phân biệt kí tự hoa hay thường
+        {
+            cout << "\n\n\t\tSINH VIEN THU " << i + 1 << endl;
+            outputOneStudent(s[i]);
+        }
     }
 }
